1230ee4f2e0dcb81db822a097c10c604dc34840e
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-04-06 22:39:31 +0300

    feat: Teacher-delete page and TeacherDelete controller were added.

---
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherDeleteController.java
@@ -0,0 +1,49 @@
+package gr.aueb.cf.schoolapp.controller;
+
+import gr.aueb.cf.schoolapp.dao.ITeacherDAO;
+import gr.aueb.cf.schoolapp.dao.TeacherDAOImpl;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.exceptions.TeacherDaoException;
+import gr.aueb.cf.schoolapp.exceptions.TeacherNotFoundException;
+import gr.aueb.cf.schoolapp.service.ITeacherService;
+import gr.aueb.cf.schoolapp.service.TeacherServiceImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/school-app/teachers/delete")
+public class TeacherDeleteController extends HttpServlet {
+
+    private final ITeacherDAO teacherDAO = new TeacherDAOImpl();
+    private final ITeacherService teacherService = new TeacherServiceImpl(teacherDAO);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idParam = req.getParameter("id");
+        String message = null;
+
+        if (idParam == null || idParam.trim().isEmpty()) {
+            message = "Το ID εκπαιδευτή για την διαγραφή είναι απαραίτητο.";
+        } else {
+            try {
+                Long id = Long.parseLong(idParam.trim());
+                teacherService.deleteTeacher(id);
+                req.setAttribute("id", id);
+                req.getRequestDispatcher("/WEB-INF/jsp/teacher-deleted.jsp").forward(req, resp);
+                return;
+            } catch (NumberFormatException e) {
+                message = "Το ID πρέπει να είναι έγκυρος ακέραιος αριθμός.";
+            } catch (TeacherNotFoundException e) {
+                message = "Δεν βρέθηκε εκπαιδευτής με το συγκεκριμένο ID.";
+            } catch (TeacherDaoException e) {
+                message = "Σφάλμα κατά την ανάκτηση των στοιχείων του εκπαιδευτή.";
+            }
+        }
+        req.setAttribute("error", message);
+        req.getRequestDispatcher("/WEB-INF/jsp/teacher-deleted.jsp").forward(req, resp);
+    }
+}
